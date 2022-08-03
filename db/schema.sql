e3e992916f666fbd6e90bf487d3dde68c3174844
Carolina Hernandez <kpresents.0@gmail.com>
2022-08-02 19:11:56 -0700

    drafting schema, index.js and user.js

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS travels_db;
+
+CREATE DATABASE travels_db;
+
+USE travels_db;
+
+CREATE TABLE    (
+
+
+
+);
---
+++ models/user.js
@@ -0,0 +1,36 @@
+// const { Model, DataTypes } = require ('sequilize');
+// const sequilize= require ('../config/connections');
+
+// class User extends Model {}
+
+// User.init(
+// {
+//     id:{
+
+
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// }
+// {
+//     sequilize,
+//     freezeTableName: true,
+//     underscore: true,
+//     modelName: "user",
+
+// }
+
+// );
+// model.exports = User;
