50b6d2cd38d883f9419e3b4bce1a6f11caa2d996
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-08-03 04:08:42 -0700

    save session using user_id and log into session async

--- controllers/API/userroutes.js
+++ controllers/API/userroutes.js
@@ -2,3 +2,20 @@
 const router = require('express').Router();
 // import USER models
 const { User } = require('../../models');
+
+// save session using user_id and log into session
+router.post('/', async (req, res) => {
+    try{
+        const userProfile = await User.create(req.body);
+        // save session
+        req.session.save(()=> {
+            req.session.user_id = userProfile.id;
+            req.session.logged_in = true;
+            // return res 200 network status
+            res.status(200).json(userProfile);
+        });
+    // else catch error and return res 400 network status
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
