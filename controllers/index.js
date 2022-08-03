e0bde774db40cbc9f09526bc31be68132b3837eb
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-08-02 18:24:55 -0700

    .env updated database Name, index.js controller updated to import userRoutes/homeRoutes, schema.sql updated to create travels_DB

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,10 @@
+// require express.router
+const router = require('express').Router();
 // connect user and home routes
+const apiRoutes = require('./api');
+
+// use middleware
+router.use('/api', apiRoutes);
+
+// export module as router
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+--delete database if exists
+DROP DATABASE IF EXISTS tavels_DB;
+-- create database
+CREATE DATABASE travels_DB;
