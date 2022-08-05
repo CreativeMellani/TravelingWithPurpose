aa1c393deaa9973db23c7d7b9cd3c9e5425684d1
CreativeMellani <101590432+CreativeMellani@users.noreply.github.com>
2022-08-04 17:36:28 -0700

    add log in modal and button functionality.

--- public/script.js
+++ public/script.js
@@ -11,3 +11,20 @@
     })
 })(); 
 
+
+// Modal functionality
+const signupButton = document.querySelector('#signUp');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal')
+
+signupButton.addEventListener('click', () => {
+    modal.classList.add('is-active');
+});
+
+// Adding event listener to close background of modal
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+});
+
+
+
