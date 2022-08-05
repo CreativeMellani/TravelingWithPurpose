58e0c4a5943fbb18212aaceed9fc8b8550051513
Carolina Hernandez <kpresents.0@gmail.com>
2022-08-04 20:43:30 -0700

    updated seeds, creataed searched.js and added handlebars

---
+++ models/searched.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connections');
+
+class Searched extends Model {}
+
+Searched.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // description: {
+    //   type: DataTypes.STRING,
+    // },
+    date_searched: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'searched',
+  }
+);
+
+module.exports = Searched;
